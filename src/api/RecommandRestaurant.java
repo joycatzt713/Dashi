b565c1653f633ddedeb494051eb294a901c02e2d
joycatzt713 <joycatzt@gmail.com>
2016-09-22 20:14:44 -0700

    First commit

---
+++ src/api/RecommandRestaurant.java
@@ -0,0 +1,72 @@
+package api;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import db.DBConnection;
+import db.MongoDBConnection;
+import db.MySQLDBConnection;
+
+/**
+ * Servlet implementation class RecommandRestaurant
+ */
+@WebServlet("/recommendation")
+public class RecommandRestaurant extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RecommandRestaurant() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    DBConnection connection = new MySQLDBConnection();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//JSONArray array = null;
+		// allow access only if session exists
+				HttpSession session = request.getSession();
+				if (session.getAttribute("user") == null) {
+					response.setStatus(403);
+					return;
+				}
+		JSONArray array = new JSONArray();
+		if (request.getParameterMap().containsKey("user_id")) {
+			String userId = (String)session.getAttribute("user");
+//			String userId = request.getParameter("user_id");
+			array = connection.recommendRestaurants(userId);
+		}
+		RpcParser.writeOutput(response, array);
+
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// allow access only if session exists
+				HttpSession session = request.getSession();
+				if (session.getAttribute("user") == null) {
+					response.setStatus(403);
+					return;
+				}
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
